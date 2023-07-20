202095041a6715444185cf2bec8a7cb301d21b5f
Matthias Folz <folz.matthias@googlemail.com>
2023-07-20 20:14:02 +0200

    silent unused interface methods and optimize initializers

--- Harmonic.h
+++ Harmonic.h
@@ -1,35 +1,38 @@
 
+#ifndef HARMONIC
+
+#define HARMONIC
+
 #include "math.h"
 
 namespace Harmonic {
 
-static const unsigned int  max_notes = 127;
+static const unsigned int max_notes = 127;
 
-static float        NOTES[max_notes];
+static float NOTES[max_notes];
 static unsigned int PERIODS[max_notes];
 
 // Frequency of C0 in hertz
-static const float  baseFreq = 16.3516;
+static const float baseFreq = 16.3516;
 // Ratio between consecutive frequencies (C1 to C2, C2 to C3, etc.)
-static const double ratio    = 1.0594630943593;
-
-
+static const double ratio = 1.0594630943593;
 
 static float calculateHertz(unsigned short note) {
-    return baseFreq * pow(ratio, note);
+  return baseFreq * pow(ratio, note);
 }
 
 static void calculate(unsigned short offset, unsigned long factor) {
-    for (unsigned short note = 0; note <= max_notes; note++) {
-        NOTES[note]   = calculateHertz(note + offset) ;
-        PERIODS[note] = ((1 / NOTES[note]) * factor);
-    }
+#ifdef DEBUG
+  Serial.println("Harmonic.calculate();");
+#endif
+  for (unsigned short note = 0; note < max_notes; note++) {
+    NOTES[note] = calculateHertz(note + offset);
+    PERIODS[note] = (unsigned int)((1 / NOTES[note]) * factor);
+  }
 }
-static void calculate(unsigned short offset) {
-    calculate(offset, pow(10,6)) ;
-}
+static void calculate(unsigned short offset) { calculate(offset, pow(10, 6)); }
 
-static void calculate() {
-    calculate(0) ;
-}
-}
+[[maybe_unused]] static void calculate() { calculate(0); }
+} // namespace Harmonic
+
+#endif
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Harmonic is an little C++ Library to Calculate All harmonic tones.
 The notes are limited to the max. number the MIDI-Protocol can handle *(127)*.
 
 
-The calculation storesthe note scale  as Hertz in the NOTES-Array and as Periods in
+The calculation stores the note scale  as Hertz in the NOTES-Array and as Periods in
 PERIODS-Array. The offset parameter make it possible to shift the calculation per notes.
 
 The factor will be used to calculate the Periods. The default is 10^6.
